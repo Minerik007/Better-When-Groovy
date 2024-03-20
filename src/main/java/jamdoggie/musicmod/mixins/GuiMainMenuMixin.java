96560bc707c3e99d0eac488dea00ab622b625e29
JamDoggie <JamDoggie3@gmail.com>
2024-03-20 08:08:49 -0400

    Add C418 bandcamp link & update icon.

--- src/main/java/jamdoggie/musicmod/mixins/GuiMainMenuMixin.java
+++ src/main/java/jamdoggie/musicmod/mixins/GuiMainMenuMixin.java
@@ -3,14 +3,16 @@ package jamdoggie.musicmod.mixins;
 import jamdoggie.musicmod.mixininterface.ISoundManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.sound.SoundManager;
+import net.minecraft.core.lang.I18n;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = GuiMainMenu.class, remap = false)
-public class GuiMainMenuMixin
+public abstract class GuiMainMenuMixin extends GuiScreen
 {
 	@Inject(method = "tick", at = @At("TAIL"))
 	private void tick(CallbackInfo ci)
@@ -23,5 +25,14 @@ public class GuiMainMenuMixin
 		{
 			mixinSndManager.playTitleScreenMusic();
 		}
+	}
+
+	@Inject(method = "drawScreen", at = @At("TAIL"))
+	private void drawScreen(int mouseX, int mouseY, float partialTick, CallbackInfo ci)
+	{
+		I18n i18n = I18n.getInstance();
+
+		String c418Disclaimer = i18n.translateKey("musicmod.titledisclaimer");
+		this.drawString(this.fontRenderer, c418Disclaimer, this.width - this.fontRenderer.getStringWidth(c418Disclaimer) - 2, this.height - 30, 0xFFFFFF);
 	}
 }
